594ac29e3d71a313b934957028318db0849c6f07
sam.yang <sam.yang@tktech.org>
2025-01-09 21:44:24 +0800

    Initialize React project with Vite

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'build', // 設定打包輸出目錄
+  }
+})
